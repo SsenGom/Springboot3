c266e4ad8f6ee6be0b988b00a2cc4f3b5c89922f
bjh <bjtiger516@naver.com>
2023-12-26 23:15:47 +0900

    sts3 첫삽

--- src/main/java/org/example/springbootdeveloper/SpringBootDeveloperApplication.java
+++ src/main/java/org/example/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootDeveloperApplication {
+public class
+SpringBootDeveloperApplication {
     public static void main(String[] args){
         SpringApplication.run(SpringBootDeveloperApplication.class,args);
     }
